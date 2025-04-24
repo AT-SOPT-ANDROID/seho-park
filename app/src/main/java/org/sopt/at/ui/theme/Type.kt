0d4c2c24e0d1289781db3b2d3dd45053e16ecf0a
sayyyho <323psh@naver.com>
2025-04-25 02:18:27 +0900

    [setting] #3 폰트 세팅

--- app/src/main/java/org/sopt/at/ui/theme/Type.kt
+++ app/src/main/java/org/sopt/at/ui/theme/Type.kt
@@ -1,34 +1,53 @@
 package org.sopt.at.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import org.sopt.at.R
 
-// Set of Material typography styles to start with
-val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
+val tivingBold = FontFamily(Font(R.font.pretendard_bold))
+val tivingSemiBold = FontFamily(Font(R.font.pretendard_semibold))
+val tivingRegular = FontFamily(Font(R.font.pretendard_regular))
+
+@Immutable
+data class TivingTypography(
+    val title: TextStyle,
+    val subTitle: TextStyle,
+    val body: TextStyle,
+    val button: TextStyle,
+    val caption: TextStyle,
 )
+
+val defaultTivingTypography = TivingTypography(
+    title = TextStyle(
+        fontFamily = tivingBold,
+        fontSize = 24.sp,
+        lineHeight = 32.sp
+    ),
+    subTitle = TextStyle(
+        fontFamily = tivingSemiBold,
+        fontSize = 18.sp,
+        lineHeight = 26.sp
+    ),
+    body = TextStyle(
+        fontFamily = tivingRegular,
+        fontSize = 16.sp,
+        lineHeight = 24.sp
+    ),
+    button = TextStyle(
+        fontFamily = tivingBold,
+        fontSize = 14.sp,
+        lineHeight = 20.sp
+    ),
+    caption = TextStyle(
+        fontFamily = tivingRegular,
+        fontSize = 12.sp,
+        lineHeight = 16.sp
+    ),
+)
+
+val LocalTivingTypographyProvider = staticCompositionLocalOf { defaultTivingTypography }
