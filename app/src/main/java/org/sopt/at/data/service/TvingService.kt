fdfb3393e4c4c065f32ae9b1346d6fe56f2f6a70
sayyyho <323psh@naver.com>
2025-05-10 04:41:57 +0900

    [feature] #7 Service 구현

--- app/src/main/java/org/sopt/at/data/service/TvingService.kt
+++ app/src/main/java/org/sopt/at/data/service/TvingService.kt
@@ -1,13 +1,29 @@
 package org.sopt.at.data.service
 
-import org.sopt.at.data.dto.response.ResponseUserListDto
-import retrofit2.Call
+import org.sopt.at.data.dto.base.BaseResponse
+import org.sopt.at.data.dto.request.SignInRequest
+import org.sopt.at.data.dto.request.SignUpRequest
+import org.sopt.at.data.dto.response.MyNicknameResponse
+import org.sopt.at.data.dto.response.SignInResponse
+import org.sopt.at.data.dto.response.SignUpResponse
+import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.Header
+import retrofit2.http.POST
 
 interface TvingService {
-    @GET("/api/users")
-    fun getListUser(
-        @Query("page") page: Int
-    ): Call<ResponseUserListDto>
+    @POST("/api/v1/auth/signup")
+    suspend fun postSignUp(
+        @Body request: SignUpRequest
+    ): BaseResponse<SignUpResponse>
+
+    @POST("/api/v1/auth/signin")
+    suspend fun postSignIn(
+        @Body request: SignInRequest
+    ): BaseResponse<SignInResponse>
+
+    @GET("/api/v1/users/me")
+    suspend fun getUserNickname(
+        @Header("userId") userId: Long
+    ): BaseResponse<MyNicknameResponse>
 }
